a0038bc43db209c37c547d7134fa28fce5234ca1
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-05-29 14:32:51 +0100

    Create SortedSetCities.cs with initial setup
    Initial implementation of a SortedSet for cities with TODOs for adding, displaying, and updating cities.

---
+++ 08-SortedSet/SortedSetCities.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Create the dictionary
+        SortedSet<string> cidades = new SortedSet<string>{"Porto"};
+
+        // TODO: Add 3 cities that you visit
+        
+        // TODO: Display all visited cities
+        
+        // TODO: Check if the city Eindhoven is in the visited cities
+        
+        // TODO: Replace the city of Porto by the city of London
+
+        // TODO: Display updated dictionary
+    }
+}
