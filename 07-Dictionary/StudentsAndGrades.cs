11b9ec5f04f8e2f614bb22715566968dc7f96090
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-05-29 14:24:11 +0100

    Initialize StudentsAndGrades.cs with dictionary setup
    Set up a basic structure for managing student grades using a dictionary.

---
+++ 07-Dictionary/StudentsAndGrades.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Create the dictionary
+        Dictionary<string, double> grades = new Dictionary<string, double>();
+
+        // TODO: Add 3 students and grades
+        
+        // TODO: Display all students and grades
+        
+        // TODO: Calculate average grade
+
+        // TODO: Display the student with highest grade
+
+        // TODO: Update a student's grade
+
+        // TODO: Display updated dictionary
+    }
+}
